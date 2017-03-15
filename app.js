a1b6504253d854f2d31824ebab20e4c807099cef
ageronimo <angelageroniimo@gmail.com>
2017-03-15 09:11:33 -1000

    added more to the simplified json

--- app.js
+++ app.js
@@ -41,12 +41,29 @@ var jsonDataBody = [
 
 var simplifiedJSON = [
   {
+    "identification": {
+      "id": "first",
+      "class": "content"
+    }
     "journalEntry": "1",
     "date": "August 15, 2016",
-    "title": "Purpose", 
-    "article": [
-      "Why I took this class: I took this class because I want to learn how to program. I have a couple of friends that know some programming languages like HTML and Java. They were my inspiration to take this class because I aspire to reach or possibly exceed them in programming. This class would certainly be a big help. I also wanted to be one of the first people to take the class since this is the first time it is being offered to a public school in Hawai'i.", "What I want to get out of this class: "
-    ]
+    "title": "My First Entry", 
+    "article": {
+      "h3": [
+        {
+          "class": "header",
+          "content": "Why I took this class:"
+        },
+        {
+          "class": "header",
+          "content": "What I want to get out of this class:"
+        }
+      ],
+      "p": [
+        "I took this class because I want to learn how to program. I have a couple of friends that know some programming languages like HTML and Java. They were my inspiration to take this class because I aspire to reach or possibly exceed them in programming. This class would certainly be a big help. I also wanted to be one of the first people to take the class since this is the first time it is being offered to a public school in Hawai'i.", 
+        "By the end of this school year, I want to be able to program fluently in at least two programming languages. I also want to be able to program a robot for First Robotics Competition (FRC) using Javascript. Doing so would allow me to utilize what I learned and make a contribution to my robotics team."
+      ]
+    }
     "img": undefined
   }
-]
+];
