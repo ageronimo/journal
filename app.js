5804d8b560309ca4af8fff66dd00a70cf310fa21
ageronimo <angelageroniimo@gmail.com>
2017-03-15 09:08:13 -1000

    wrote json and simplified json

---
+++ app.js
@@ -0,0 +1,52 @@
+var jsonDataBody = [
+  {
+    "header":["h1", "h4", "img", "hr"]
+  },
+  //if there is a class on the attribute, make it an object
+];
+
+// var entryOne = [
+//   {
+//     "div": {
+//       "id": "first",
+//       "class": "content",
+//       [
+//         "h2", 
+//         "b", 
+//         "span", 
+//         "div": {
+//           "class":"article",
+//           [ 
+//             "h3":{
+//               "class":"header";
+//             }, 
+//             "p"
+//           ]
+//         }
+//         "div": {
+//           "class" : "article",
+//           [
+//             "h3", 
+//             "p", 
+//             "span": {
+//               "class": "highlight"
+//             }
+//           ]
+//         }
+//       ]
+//     }
+//   }
+// ];
+
+
+var simplifiedJSON = [
+  {
+    "journalEntry": "1",
+    "date": "August 15, 2016",
+    "title": "Purpose", 
+    "article": [
+      "Why I took this class: I took this class because I want to learn how to program. I have a couple of friends that know some programming languages like HTML and Java. They were my inspiration to take this class because I aspire to reach or possibly exceed them in programming. This class would certainly be a big help. I also wanted to be one of the first people to take the class since this is the first time it is being offered to a public school in Hawai'i.", "What I want to get out of this class: "
+    ]
+    "img": undefined
+  }
+]
